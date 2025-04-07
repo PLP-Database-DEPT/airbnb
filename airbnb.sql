8ca2a974369aed9e3fe80f303215781ea9343cfc
Randa_Mike <michaelranda95@gmail.com>
2025-04-07 15:22:41 +0300

    created booking table with relationships

--- airbnb.sql
+++ airbnb.sql
@@ -11,3 +11,42 @@ CREATE TABLE attribute_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
 );
+
+
+
+-- creates table reviews with relationships
+-- creats properties table
+CREATE TABLE properties (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    categories_name VARCHAR(100) 
+);
+
+-- creates users table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) 
+);
+
+-- creates booking_status table
+CREATE TABLE booking_status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    status_name VARCHAR(50) 
+);
+
+-- creates table booking with relationships
+CREATE TABLE bookings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    booking_status_id INT NOT NULL,
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL,
+    nightly_price DECIMAL(10, 2) NOT NULL,
+    service_fee DECIMAL(10, 2),
+    cleaning_fee DECIMAL(10, 2),
+    total_price DECIMAL(10, 2),
+    
+    FOREIGN KEY (property_id) REFERENCES properties(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (booking_status_id) REFERENCES booking_status(id)
+);
