c7ef01537d12238f184d1773682b472838cef3d3
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-04-07 12:12:53 +0300

    feat: added the countries table

--- airbnb.sql
+++ airbnb.sql
@@ -11,3 +11,14 @@ CREATE TABLE attribute_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
 );
+
+-- Creating countries table
+-- ensures no duplicate entries for the countries
+-- for this to work effectively,the table region must exist
+CREATE TABLE country (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    region_id INT NOT NULL,
+    country_name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT,
+    FOREIGN KEY (region_id) REFERENCES region(id)
+);
