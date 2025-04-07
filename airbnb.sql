2310840ef832af39dce24622a0ce6d7e1f0ddd1d
lonyamasi <atyangasila@gmail.com>
2025-04-07 15:39:07 +0300

    Created property-category table, with both columns containing foreignkeys

--- airbnb.sql
+++ airbnb.sql
@@ -11,3 +11,14 @@ CREATE TABLE attribute_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
 );
+
+
+---Create the property_category table with property and category id being foreign keys from propert and category tables
+
+CREATE TABLE property_category (
+    property_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (property_id, category_id),
+    FOREIGN KEY (property_id) REFERENCES property(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
