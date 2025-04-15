12877c12ffb8a8c9218f2e118d76421a922fbfe4
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-04-15 15:12:17 +0300

    created guest_type table

--- airbnb.sql
+++ airbnb.sql
@@ -1,4 +1,5 @@
 CREATE TABLE guest_type(
 id INT PRIMARY KEY,
-type_name varchar(50) NOT NULL
+type_name varchar(50) NOT NULL,
+FOREIGN KEY (id) REFERENCES booking_guests(booking_id) ON DELETE CASCADE 
 );
