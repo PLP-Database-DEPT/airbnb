e9fd7fd9410d459f239bd859625345268324e08a
Ian12467 <ianochieng277@gmail.com>
2025-04-22 17:29:58 +0300

    fix on the sql

--- airbnb.sql
+++ airbnb.sql
@@ -1,17 +1,3 @@
--- create airbnb database
-CREATE DATABASE airbnb;
-
-CREATE TABLE category(
-id INT PRIMARY KEY,
-category_name varchar(50) NOT NULL
-);
-
--- Create attribute_category table
-CREATE TABLE attribute_category (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL
-);
-
 -- Place Type
 CREATE TABLE place_type (
     id INT PRIMARY KEY AUTO_INCREMENT,
