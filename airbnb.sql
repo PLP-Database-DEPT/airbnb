83b2d356e024d5a27beadc59f64f49ef21f6fd9d
mutheeEverlyn <123722613+mutheeEverlyn@users.noreply.github.com>
2025-04-23 14:34:49 +0300

    Guest type table created (#43)
    * created category table
    
    * category table
    
    * created guest_type table
    
    * created guest_type table
    
    * created guest_type table
    
    ---------
    
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- airbnb.sql
+++ airbnb.sql
@@ -1,3 +1,9 @@
+CREATE TABLE guest_type(
+id INT PRIMARY KEY,
+type_name varchar(50) NOT NULL,
+FOREIGN KEY (id) REFERENCES booking_guests(booking_id)
+);
+
 CREATE TABLE location (
 id INT AUTO_INCREMENT PRIMARY KEY,
 country_id INT NOT NULL,
