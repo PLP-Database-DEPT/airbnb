3ec8ab276890b894274bdd24f5f14ef45a01fa01
lonyamasi <atyangasila@gmail.com>
2025-04-23 13:34:38 +0300

    created review component table

--- airbnb.sql
+++ airbnb.sql
@@ -11,3 +11,14 @@ CREATE TABLE attribute_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
 );
+
+-- Create the review_component table with the following columns:
+-- id (int)
+-- component_name (varchar)
+
+-- Ensure that the id is unique for each row.
+
+CREATE TABLE review_component (
+    id INT PRIMARY KEY,
+    component_name VARCHAR(255) NOT NULL
+);
