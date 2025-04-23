d4f3401d1cbf19dd3e0b2f329a8fd9cf683c453f
Bella-winter <polinmwangi6115@gmail.com>
2025-04-23 14:33:33 +0300

    Add location table to airbnb.sql (#27)
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- airbnb.sql
+++ airbnb.sql
@@ -1,8 +1,16 @@
+CREATE TABLE location (
+id INT AUTO_INCREMENT PRIMARY KEY,
+country_id INT NOT NULL,
+location_name varchar(255) NOT NULL,
+FOREIGN KEY (country_id) REFERENCES country(id)
+);
+
 CREATE TABLE booking_status (
 id INT AUTO_INCREMENT PRIMARY KEY,  
 status_name VARCHAR(50) NOT NULL,    
 UNIQUE (status_name)
 );
+
 
 -- Place Type
 CREATE TABLE place_type (
