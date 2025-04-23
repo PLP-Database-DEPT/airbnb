93b42743df2dd7930ba7b04a6aff0c1ae6345972
Janemutegi <139335845+Janemutegi@users.noreply.github.com>
2025-04-23 14:26:58 +0300

    user_account table (#39)
    * updated file
    
    * changes effected
    
    ---------
    
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- airbnb.sql
+++ airbnb.sql
@@ -1,3 +1,12 @@
+CREATE TABLE user_account (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_Name VARCHAR(50) NOT NULL,
+    last_Name VARCHAR(50) NOT NULL,
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    user_password VARCHAR(50) NOT NULL,
+    joined_date DATE NOT NULL,
+    date_host_started DATE NULL
+);
 -- Create the region table
 CREATE TABLE region(
 id INT AUTO_INCREMENT PRIMARY KEY,
