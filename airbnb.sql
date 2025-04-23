6d26f2aed8b462a1cec29ece0d05654d73a8bb45
Michael Randa <michaelranda95@gmail.com>
2025-04-23 14:20:54 +0300

    created booking table with relationships (#36)
    * created booking table with relationships
    
    * modified the bookings table
    
    ---------
    
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- airbnb.sql
+++ airbnb.sql
@@ -9,6 +9,24 @@ CREATE TABLE attribute_category (
     category_name VARCHAR(255) NOT NULL
 );
 
+
+-- creates table booking with relationships
+CREATE TABLE bookings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    booking_status_id INT NOT NULL,
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL,
+    nightly_price DECIMAL(10, 2) NOT NULL,
+    service_fee DECIMAL(10, 2),
+    cleaning_fee DECIMAL(10, 2),
+    total_price DECIMAL(10, 2),
+    
+    FOREIGN KEY (property_id) REFERENCES properties(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (booking_status_id) REFERENCES booking_status(id)
+);
 CREATE TABLE country (
     id INT AUTO_INCREMENT PRIMARY KEY,
     region_id INT NOT NULL,
@@ -39,3 +57,4 @@ CREATE TABLE property (
     FOREIGN KEY (property_type_id) REFERENCES property_type(id),
     FOREIGN KEY (host_id) REFERENCES host(id)
   );
+
